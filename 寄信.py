1fafeee933de3a38996fbf86675e624cdf3d2105
iamernie8199 <iamernie8199@gmail.com>
2024-05-08 16:34:49 +0800

    Update 寄信.py

--- 寄信.py
+++ 寄信.py
@@ -7,9 +7,15 @@ newmail = ol.CreateItem(olmailitem)
 
 newmail.Subject = 'Testing Mail'
 newmail.To = 'xyz@gmail.com'
-newmail.CC = 'xyz@gmail.com'
+newmail.CC = 'xyz@gmail.com; abc@gmail.com'
 
-newmail.Body = 'Hello, this is a test email to showcase how to send emails from Python and Outlook.'
+# newmail.Body = 'Hello, this is a test email to showcase how to send emails from Python and Outlook.'
+
+newmail.Attachments.Add(os.path.join(os.getcwd(), '123.png')).PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "123")
+body = "<b>blablabla</b><br><br>"
+body += """<img src="cid:123"  width="800"><br>"""
+newmail.HTMLBody = body
+
 # attach='C:\\Users\\admin\\Desktop\\Python\\Sample.xlsx'
 # newmail.Attachments.Add(attach)
 
