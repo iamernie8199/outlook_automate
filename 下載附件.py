a2f4eb6373b35f20f462004181e7cf5f8ede7b14
iamernie8199 <iamernie8199@gmail.com>
2024-12-09 16:06:16 +0800

    新增關鍵字搜尋

--- 下載附件.py
+++ 下載附件.py
@@ -3,11 +3,16 @@ import os
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 # 收件匣
-inbox = outlook.GetDefaultFolder(6)
+inbox = outlook.GetDefaultFolder(6) # 預設資料夾
+# inbox = outlook.Folders.Item(1).Folders['收件匣'] # 備份資料夾
+
 messages = inbox.Items
 # messages = outlook.Folders.Item("Inbox").Items
+# messages = inbox.Items.Restrict("@SQL=""http://schemas.microsoft.com/mapi/proptag/0x0E1D001F"" like '%abc%' ") # 搜尋關鍵字
+
 # 倒序排序
-messages = [m for m in messages][::-1]
+# messages = [m for m in messages][::-1]
+messages.Sort("[ReceivedTime]", True)
 
 for m in messages:
     # 搜尋特定標題
